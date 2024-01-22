38f68963c45b6ad4a3575b14bf5d129e1dc5b637
gh0stfrk <salmansyyd12@gmail.com>
2024-01-22 23:39:37 +0530

    updt:Add structure

---
+++ get_books.py
@@ -0,0 +1,56 @@
+import os
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+
+prompt = """
+Your name is Book Genie and you are a book recommender you will recommend books based on the input from the user try to recommed books that will cover everyting that was provided in the input
+
+In response you will only return a json object with 10 books in a json format like this
+{
+    {
+        "book_name":"Book Name",
+        "author_name":"Author",
+        "year": "year of publishing",
+        "reason": "reason of suggesting this book"
+    },
+    {
+        "book_name":"Book Name",
+        "author_name":"Author",
+        "year": "year of publishing",
+        "reason": "reason of suggesting this book"
+    }
+}
+"""
+
+def getBooks(query:str):
+    """
+    Dictionary of books from OpenAI
+
+    :param query: str
+    :return: dict
+    """
+    client = OpenAI()
+    compeletion = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    response_format={ "type": "json_object" },
+    messages=[
+        {
+            "role":"system",
+            "content": prompt
+        },
+        {
+            "role":"user",
+            "content":f"{query}"
+        }
+    ]
+    )
+
+    response_msg = compeletion.choices[0].message.content
+    parsed_json = json.loads(response_msg)
+    return parsed_json
--- readme.md
+++ readme.md
@@ -4,3 +4,26 @@ API's for book genie
 # Featuers 
 - Rate limitting 
 - Allow more queries after signup with google
+
+# Endpoints
+
+getbookRecommendations() POST
+Req ["query",auth_token|temp_user,]
+{
+    returns a list of books with required keys
+    {
+        "bookName",
+        "author",
+        "coverImg:optional",
+        "description",
+        "affiliate_link",
+    }
+}
+
+
+# Steps to get return a bookRecommendations JSON obj
+
+- 1) Get the msg from the user to suggest books
+- 2) Get a list of books from OpenAi API
+- 3) Find affliate links to those books from ebooks.com
+- 4) Create a new object with all necessary items to dispay user books
